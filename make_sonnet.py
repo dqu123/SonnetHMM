5091aa1b48b3936c37b4cbbac8a82f7011b2fad5
dqu123 <davidqu12345@gmail.com>
2017-02-22 21:41:40 -0800

    Test hmm training on smaller dataset

--- make_sonnet.py
+++ make_sonnet.py
@@ -1,6 +1,13 @@
 import argparse
+from sonetto.HMM import unsupervised_HMM
+from sonetto.parser import SonnetParser
 
 DATA_DIR = 'data/'
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Makes Shakespearean Sonnets')
+    sp = SonnetParser()
+    sp.parse('{}{}.txt'.format(DATA_DIR, 'mini_shakespeare'))
+
+    hmm = unsupervised_HMM(sp, 10)
+    print hmm.generate_emission(84)
