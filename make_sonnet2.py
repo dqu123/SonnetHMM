3080cb65ffa6c511ce277eb4540ebf43e37ef466
albert <age@caltech.edu>
2017-02-26 14:05:01 -0800

    added LSTM implementation

--- make_sonnet2.py
+++ make_sonnet2.py
@@ -1,4 +1,5 @@
 import argparse
+import numpy as np
 
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
@@ -9,7 +10,7 @@ from keras.utils.data_utils import get_file
 from sonetto.parser import SonnetParser
 
 DATA_DIR = 'data/'
-FILE = 'mini_shakespeare'
+FILE = 'behemoth'
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Makes Shakespearean Sonnets')
@@ -22,20 +23,63 @@ if __name__ == '__main__':
     sp = SonnetParser()
     sp.parse('{}{}.txt'.format(DATA_DIR, FILE))
 
-    SEQ_LEN = 15
-    flatten = ' '.join(sp.lines)
-    for word in flatten.split():
+    seed = "from fairest creatures we desire increase that thereby beauty's rose might never die".split(' ')
+    print seed
+    SEQ_LEN = len(seed)
+    data = [item for sublist in sp.lines for item in sublist]
+    print data
 
+    seq = []
+    nxt = []
+    step = 5
+    for i in range(0, len(data) - SEQ_LEN, step):
+        seq.append(data[i:i + SEQ_LEN])
+        nxt.append(data[i + SEQ_LEN])
+
+
+
+    inp = np.zeros(((len(seq), SEQ_LEN, sp.word_count)))
+    out = np.zeros(((len(seq), sp.word_count)))
+
+    for i in range(len(seq)):
+        for j in range(len(seq[i])):
+            inp[i][j][sp.word_to_num[seq[i][j]]] = 1
+
+        out[i][sp.word_to_num[nxt[i]]] = 1
 
 
     model = Sequential()
-    model.add(LSTM(128, input_shape = (SEQ_LEN, len(char))))
-    model.add(Dense(len(char)))
+    model.add(LSTM(128, input_shape = (SEQ_LEN, sp.word_count)))
+    model.add(Dense(sp.word_count))
     model.add(Activation('softmax'))
 
     optimizer = RMSprop(lr = 0.001)
-    model.compile(loss = 'categorical_crossentropy', optimizer)
+    model.compile(loss = 'categorical_crossentropy', optimizer =optimizer)
 
+    model.fit(inp, out, batch_size=128, nb_epoch=20, verbose=2)
+    newSeed = list(seed)
+    generated = list(seed)
+    for i in range(100):
+        genInput = np.zeros((1, SEQ_LEN, sp.word_count))
+        for j in range(len(newSeed)):
+            genInput[0][j][sp.word_to_num[newSeed[j]]] = 1 
+
+        dist = model.predict(genInput)[0]
+        dist = np.asarray(dist).astype('float64')
+        logs = np.log(dist)
+        softmax = np.exp(logs) / np.sum(np.exp(logs))
+        multi = np.random.multinomial(1, softmax)
+        sample = np.argmax(multi)
+
+        pred = sp.num_to_word[sample]
+
+        generated.append(pred)
+
+        newSeed = newSeed[1: ]
+        newSeed.append(pred)
+
+
+    print ' '.join(generated)
 
 
 
